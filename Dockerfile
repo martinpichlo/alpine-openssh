0dd906a4dd1a398be6267c4648ec21caa66ba27a
Zeaq <m dot pichlo at gmx dot de>
2020-01-13 23:14:41 +0100

    ready

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
 #based on /alpine-sshd/dockerfile and others
 FROM alpine:latest
 MAINTAINER Martin Pichlo <m.pichlo@gmx.de>
-
-# add openssh and clean
-RUN apk add --update openssh \
-&& rm  -rf /tmp/* /var/cache/apk/*
-# add entrypoint script
 ADD entrypoint.sh /usr/local/bin
-#make sure we get fresh keys
-RUN rm -rf /etc/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_dsa_key
 
-#inherit permissions
-#VOLUME /root/.ssh
+RUN apk add --update openssh && rm  -rf /tmp/* /var/cache/apk/* && \
+    rm -rf /etc/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_dsa_key && \
+    cat > /etc/motd
 
 EXPOSE 22
 ENTRYPOINT ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,27 @@
 #!/bin/sh
 
-# generate fresh rsa key
-if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
-	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+# generate all host keys if not present
+if [ ! -f "ssh_host_ecdsa_key.pub" ]; then
+	ssh-keygen -A
 fi
 
-# generate fresh dsa key
-if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
-	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-fi
-
-#prepare run dir
-if [ ! -d "/var/run/sshd" ]; then
-  mkdir -p /var/run/sshd
-fi
-
-#prepare config
+# setup logon methods
 if [ ! -f "/etc/ssh/sshd_config_org" ]; then
-  cp /etc/ssh/sshd_config /etc/ssh/sshd_config_org
-# sed -i "s/#PermitRootLogin.*/PermitRootLogin without-password/" /etc/ssh/sshd_config
-# sed -i "s/#PermitRootLogin.*/PermitRootLogin prohibit-password/" /etc/ssh/sshd_config
-# sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
-  sed -i "s/GatewayPorts no/GatewayPorts yes/" /etc/ssh/sshd_config
-  sed -i "s/#ClientAliveInterval 0/ClientAliveInterval 10/" /etc/ssh/sshd_config
+
+	# sshd config
+	cp /etc/ssh/sshd_config /etc/ssh/sshd_config_org
+	sed -i "s/#PermitRootLogin.*/PermitRootLogin prohibit-password/" /etc/ssh/sshd_config
+	sed -i "s/#PasswordAuthentication.*/PasswordAuthentication no/" /etc/ssh/sshd_config
+	sed -i "s/GatewayPorts.*/GatewayPorts yes/" /etc/ssh/sshd_config
+	sed -i "s/#ClientAliveInterval.*/ClientAliveInterval 10/" /etc/ssh/sshd_config
+
+	# unlock root user
+	passwd -u root
 fi
 
-#workarround
-if [ ! -f "/root/.ssh/authorized_keys" ]; then
-  cp /root/.ssh/authorized_keys_org /root/.ssh/authorized_keys
-  chown root:root /root/.ssh/authorized_keys
+# prepare run dir
+if [ ! -d "/var/run/sshd" ]; then
+	mkdir -p /var/run/sshd
 fi
 
 exec "$@"
